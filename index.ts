88de3c2f2dbde71e21165fb2813ae7c711c66ce9
tjmuscles <tjmuscles@yahoo.com>
2022-07-15 10:59:06 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
+import { fromEvent } from "rxjs";
+import { map, concatMap, tap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
 const endpointInput: HTMLInputElement = document.querySelector('input#endpoint');
 const fetchButton = document.querySelector('button#fetch');
 
+fromEvent(fetchButton, 'click')
+.pipe(
+  map(() => endpointInput.value),
+  concatMap(value => 
+    ajax(`https://random-data-api.com/api/${value}/random_${value}`)
+  )
+)
+.subscribe(
+  value => console.log(value)
+);
